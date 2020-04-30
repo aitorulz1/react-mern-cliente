677a9c977ab99ec7a4cc3f8b8ce120e71ed5ede6
Aitor Arina <aitorulz1@gmail.com>
2020-04-30 12:49:04 +0200

    creando el state y el context de proyectos

---
+++ src/context/proyectos/proyectoState.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+import proyectoContext from './proyectoContext'
+
+
+const proyectoState = props => {
+
+    const initialState = {
+        
+        nuevoProyecto: false // Cuando cambie a true, se mostrará  presionando el boton de nuevo proyecto, el input y el boton de agrega proyecto 
+    }
+}
